d4afe9d73bd0cb6dd1faf7fa15fa1ed5aa59b29f
hilmi3x <hilmi_58_araz@hotmail.com>
2017-10-12 00:14:07 +0300

    First Map
    Created first map

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody rb;
-	public float forwardForce = 200f;
-	public float sideForce = 500f;
-	public float speedupForce = 5f;
+	public float forwardForce = 3000f;
+	public float sideForce = 80f;
+	public float speedupForce = 250f;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		rb.AddForce(0,0,forwardForce * Time.deltaTime);
 
-		if (forwardForce < 500f) {
+		if (forwardForce < 4000f) {
 			if (Input.GetKey ("w")) {
 				forwardForce = forwardForce + speedupForce;
 				rb.AddForce (0, 0, forwardForce * Time.deltaTime);
 			}
 		}
 
-		if (forwardForce > 100f) {
+		if (forwardForce > 2000f) {
 			if (Input.GetKey ("s")) {
 				forwardForce = forwardForce - speedupForce;
 				rb.AddForce (0, 0, forwardForce * Time.deltaTime);
@@ -27,11 +27,11 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		if (Input.GetKey ("d")) {
-			rb.AddForce (sideForce  * Time.deltaTime, 0, 0);
+			rb.AddForce (sideForce  * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
 		if (Input.GetKey ("a")) {
-			rb.AddForce (-sideForce  * Time.deltaTime, 0, 0);
+			rb.AddForce (-sideForce  * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 	}
 }

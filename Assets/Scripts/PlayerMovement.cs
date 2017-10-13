54bbe53b6bbc33307527fb51137e98c5e6e71409
hilmi3x <hilmi_58_araz@hotmail.com>
2017-10-13 23:45:16 +0300

    Score
    Added Score
    Still need a goal to end the game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody rb;
-	public float forwardForce = 3000f;
-	public float sideForce = 80f;
-	public float speedupForce = 250f;
+	public float forwardForce = 6000f;
+	public float sideForce = 100f;
+	public float speedupForce = 500f;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		rb.AddForce(0,0,forwardForce * Time.deltaTime);
 
-		if (forwardForce < 4000f) {
+		if (forwardForce < 8000f) {
 			if (Input.GetKey ("w")) {
 				forwardForce = forwardForce + speedupForce;
 				rb.AddForce (0, 0, forwardForce * Time.deltaTime);
 			}
 		}
 
-		if (forwardForce > 2000f) {
+		if (forwardForce > 4000f) {
 			if (Input.GetKey ("s")) {
 				forwardForce = forwardForce - speedupForce;
 				rb.AddForce (0, 0, forwardForce * Time.deltaTime);
